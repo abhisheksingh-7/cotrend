f9cc76312d01295c96330937438795efa169cd2e
Abhishek Singh <abhisheksingh.dev71@gmail.com>
2024-05-11 17:01:29 -0500

    cleanup

--- clapt/faiss/vectorstore.py
+++ clapt/faiss/vectorstore.py
@@ -1,51 +1,58 @@
 from typing import Optional
 import numpy as np
 import faiss
+import pydantic
+
+
+class IndexConfig(pydantic.BaseModel):
+    nlist: Optional[int] = 100
+    nprobe: Optional[int] = 10
+
+
+class FaissConfig(pydantic.BaseModel):
+    dim: int
+    index_config: Optional[IndexConfig] = None
+    faster_search: bool = False
+    lower_memory: bool = False
+    quantization_factor: Optional[int] = 4
 
 
 class VectorStore:
     def __init__(
         self,
-        dim: int,
-        index: Optional[faiss.Index],
+        config: FaissConfig,
         gpu_id: int,
-        faster_search: bool = False,
-        lower_memory: bool = False,
-        nlist: int = 100,
-        nprobe: int = 10,
-        quantization_factor: int = 4,
     ):
-        self.dim = dim
+        self.dim = config.dim
         self.gpu_id = gpu_id
-        self.use_ivf = faster_search
-        self.lower_memory = lower_memory
-        self.nlist = nlist
-        self.nprobe = nprobe
-        self.quantization_factor = quantization_factor
-        self.index = self.create_index(index)
+        self.faster_search = config.faster_search
+        self.lower_memory = config.lower_memory
+        self.quantization_factor = config.quantization_factor
+        if config.index_config:
+            self.nlist = config.index_config.nlist
+            self.nprobe = config.index_config.nprobe
+        self.index = self.create_index()
 
-    def create_index(self, index):
-        if self.faster_search:
-            quantizer = faiss.IndexFlatL2(self.dim)
-            if self.lower_memory:
-                index = faiss.IndexIVFPQ(
-                    quantizer,
-                    self.dim,
-                    self.nlist,
-                    self.dim / self.quantization_factor,
-                    8,
-                )
-            else:
-                index = faiss.IndexIVFFlat(
-                    quantizer, self.dim, self.nlist, faiss.METRIC_L2
-                )
-            assert not index.is_trained
-            index.train(np.random.random((self.nlist, self.dim)).astype("float32"))
+    def create_index(self):
+        index = None
+        if not self.faster_search:
+            return faiss.IndexFlatL2(self.dim)
 
+        quantizer = faiss.IndexFlatL2(self.dim)
+        if self.lower_memory:
+            index = faiss.IndexIVFPQ(
+                quantizer,
+                self.dim,
+                self.nlist,
+                self.dim // self.quantization_factor,
+                8,
+            )
         else:
-            if not index:
-                index = faiss.IndexFlatL2(self.dim)
-            index = faiss.index_factory(self.dim, index)
+            index = faiss.IndexIVFFlat(quantizer, self.dim, self.nlist, faiss.METRIC_L2)
+        assert not index.is_trained, "Index is already trained"
+        index.train(np.random.random((self.nlist, self.dim)).astype("float32"))
+
+        return index
 
     def add(self, vectors):
         self.index.add(vectors)
