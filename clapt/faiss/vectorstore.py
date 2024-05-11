04ffb80f37648b122596eb36ac6a67ea3264c642
Abhishek Singh <abhisheksingh.dev71@gmail.com>
2024-05-11 18:41:23 -0500

    add inner product

--- clapt/faiss/vectorstore.py
+++ clapt/faiss/vectorstore.py
@@ -20,6 +20,7 @@ class FaissConfig(pydantic.BaseModel):
     faster_search: bool = False
     lower_memory: bool = False
     quantization_factor: Optional[int] = 4
+    metric: int = faiss.METRIC_L2
 
 
 class VectorStore:
@@ -33,6 +34,7 @@ class VectorStore:
         self.faster_search = config.faster_search
         self.lower_memory = config.lower_memory
         self.quantization_factor = config.quantization_factor
+        self.metric = config.metric
         if config.ivf_index_config:
             self.nlist = config.ivf_index_config.nlist
             self.nprobe = config.ivf_index_config.nprobe
@@ -42,7 +44,11 @@ class VectorStore:
         # TODO: index_factory
         index = None
         if not self.faster_search:
-            index = faiss.IndexFlatL2(self.dim)
+            if self.metric == faiss.METRIC_INNER_PRODUCT:
+                # add normalized vectors
+                index = faiss.IndexFlatIP(self.dim)
+            else:
+                index = faiss.IndexFlatL2(self.dim)
             gpu_index = faiss.index_cpu_to_gpu(self.gpu_resource, self.gpu_id, index)
             return gpu_index
 
@@ -56,7 +62,7 @@ class VectorStore:
                 8,
             )
         else:
-            index = faiss.IndexIVFFlat(quantizer, self.dim, self.nlist, faiss.METRIC_L2)
+            index = faiss.IndexIVFFlat(quantizer, self.dim, self.nlist, self.metric)
         assert not index.is_trained, "Index is already trained"
 
         gpu_index: faiss.Index = faiss.index_cpu_to_gpu(
@@ -78,26 +84,49 @@ class VectorStore:
     def search(
         self, query: Union[np.ndarray, T.Tensor], k: int
     ) -> Tuple[np.ndarray, np.ndarray]:
+
         if isinstance(query, T.Tensor):
             query = query.cpu().numpy().astype("float32")
+
+        if self.metric == faiss.METRIC_INNER_PRODUCT:
+            faiss.normalize_L2(query)
         return self.index.search(query, k)
 
 
 def main() -> None:
-    faiss_config = FaissConfig(dim=128, gpu_id=0)
-    vs = VectorStore(faiss_config)
+    faiss_config_1 = FaissConfig(
+        dim=128,
+        gpu_id=0,
+        metric=faiss.METRIC_L2,
+        arbitrary_types_allowed=True,
+    )
+    faiss_config_2 = FaissConfig(
+        dim=128,
+        gpu_id=0,
+        metric=faiss.METRIC_INNER_PRODUCT,
+        arbitrary_types_allowed=True,
+    )
+    vs_1 = VectorStore(faiss_config_1)
+    vs_2 = VectorStore(faiss_config_2)
 
     nb_vectors = 1000
     k = 4
 
-    db_vectors = T.rand(nb_vectors, faiss_config.dim).cuda()
-    vs.add(db_vectors)
+    db_vectors = T.rand(nb_vectors, faiss_config_1.dim).cuda()
+    vs_1.add(db_vectors)
+    vs_2.add(db_vectors)
 
-    query_vector = T.rand(1, faiss_config.dim).cuda()
-    D, I = vs.search(query_vector, k)
+    query_vector = T.rand(1, faiss_config_1.dim).cuda()
+    logger.info(f"Query Vector: {query_vector}")
+    D_1, I_1 = vs_1.search(query_vector, k)
 
-    logger.info(f"Distances: {D}")
-    logger.info(f"Indices: {I}")
+    logger.info(f"L2 Distances: {D_1}")
+    logger.info(f"Indices: {I_1}")
+
+    D_2, I_2 = vs_2.search(query_vector, k)
+
+    logger.info(f"IP Distances: {D_2}")
+    logger.info(f"Indices: {I_2}")
 
 
 if __name__ == "__main__":
