44db705877d4795502cac4c56cd7772114a33d75
Abhishek Singh <abhisheksingh.dev71@gmail.com>
2024-05-11 19:53:54 -0500

    hnsw

--- clapt/faiss/vectorstore.py
+++ clapt/faiss/vectorstore.py
@@ -1,3 +1,4 @@
+import enum
 from typing import Optional, Tuple, Union
 import numpy as np
 import faiss
@@ -8,18 +9,35 @@ import loguru
 logger = loguru.logger
 
 
-class IVFIndexConfig(pydantic.BaseModel):
+class IVFIndexParams(pydantic.BaseModel):
     nlist: Optional[int] = 100
     nprobe: Optional[int] = 10
+    quantization_factor: Optional[int] = 4
+
+
+class HNSWIndexParams(pydantic.BaseModel):
+    M: Optional[int] = 8  # A larger M is more accurate but uses more memory
+    efConstruction: Optional[int] = 64  # depth of exploration at add time
+    efSearch: Optional[int] = 20  # depth of exploration of the search
+
+
+class IndexType(enum.Enum):
+    IVFFLAT = faiss.IndexIVFFlat
+    IVFPQ = faiss.IndexIVFPQ
+    HNSW = faiss.IndexHNSW
+    FLATIP = faiss.IndexFlatIP
+    FLATL2 = faiss.IndexFlatL2
+
+
+class IndexConfig(pydantic.BaseModel):
+    index_type: Optional[IndexType] = IndexType.FLATL2
+    index_params: Optional[Union[IVFIndexParams, HNSWIndexParams]] = None
 
 
 class FaissConfig(pydantic.BaseModel):
     dim: int
     gpu_id: int
-    ivf_index_config: Optional[IVFIndexConfig] = None
-    faster_search: bool = False
-    lower_memory: bool = False
-    quantization_factor: Optional[int] = 4
+    index_config: Optional[IndexConfig] = None
     metric: int = faiss.METRIC_L2
 
 
@@ -31,45 +49,43 @@ class VectorStore:
         self.dim = config.dim
         self.gpu_id = config.gpu_id
         self.gpu_resource = faiss.StandardGpuResources()
-        self.faster_search = config.faster_search
-        self.lower_memory = config.lower_memory
-        self.quantization_factor = config.quantization_factor
         self.metric = config.metric
-        if config.ivf_index_config:
-            self.nlist = config.ivf_index_config.nlist
-            self.nprobe = config.ivf_index_config.nprobe
+        if config.index_config:
+            self.index_type = config.index_config.index_type
+            self.index_params = config.index_config.index_params
         self.index: faiss.Index = self.create_index()
 
     def create_index(self) -> faiss.Index:
         # TODO: index_factory
         index = None
-        if not self.faster_search:
-            if self.metric == faiss.METRIC_INNER_PRODUCT:
-                # add normalized vectors
-                index = faiss.IndexFlatIP(self.dim)
-            else:
-                index = faiss.IndexFlatL2(self.dim)
-            gpu_index = faiss.index_cpu_to_gpu(self.gpu_resource, self.gpu_id, index)
-            return gpu_index
 
-        quantizer = faiss.IndexFlatL2(self.dim)
-        if self.lower_memory:
-            index = faiss.IndexIVFPQ(
-                quantizer,
-                self.dim,
-                self.nlist,
-                self.dim // self.quantization_factor,
-                8,
+        if self.index_type == IndexType.HNSW:
+            index: faiss.IndexHNSWFlat = faiss.IndexHNSWFlat(
+                self.dim, self.index_params.M, self.metric
             )
+            index.hnsw.efConstruction = self.index_params.efConstruction
+            index.hnsw.efSearch = self.index_params.efSearch
+
+        elif self.index_type in [IndexType.FLATIP, IndexType.FLATL2]:
+            index = faiss.IndexFlatIP(self.dim)
+
         else:
-            index = faiss.IndexIVFFlat(quantizer, self.dim, self.nlist, self.metric)
-        assert not index.is_trained, "Index is already trained"
+            quantizer = faiss.IndexFlatL2(self.dim)
+            if self.index_type == IndexType.IVFFLAT:
+                index = faiss.IndexIVFFlat(
+                    quantizer, self.dim, self.index_params.nlist, self.metric
+                )
+            elif self.index_type == IndexType.IVFPQ:
+                index = faiss.IndexIVFPQ(
+                    quantizer,
+                    self.dim,
+                    self.index_params.nlist,
+                    self.dim // self.index_params.quantization_factor,
+                    8,
+                )
+            index.train(np.random.rand(10000, self.dim).astype("float32"))
 
-        gpu_index: faiss.Index = faiss.index_cpu_to_gpu(
-            self.gpu_resource, self.gpu_id, index
-        )
-        gpu_index.train(np.random.random((self.nlist, self.dim)).astype("float32"))
-
+        gpu_index = faiss.index_cpu_to_gpu(self.gpu_resource, self.gpu_id, index)
         return gpu_index
 
     def add(self, vectors: Union[np.ndarray, T.Tensor]) -> None:
@@ -96,39 +112,32 @@ class VectorStore:
 
 
 def main() -> None:
-    faiss_config_1 = FaissConfig(
+    index_config = IndexConfig(
+        index_type=IndexType.IVFFLAT, index_params=IVFIndexParams()
+    )
+
+    faiss_config = FaissConfig(
         dim=128,
         gpu_id=0,
         metric=faiss.METRIC_L2,
+        index_config=index_config,
         arbitrary_types_allowed=True,
     )
-    faiss_config_2 = FaissConfig(
-        dim=128,
-        gpu_id=0,
-        metric=faiss.METRIC_INNER_PRODUCT,
-        arbitrary_types_allowed=True,
-    )
-    vs_1 = VectorStore(faiss_config_1)
-    vs_2 = VectorStore(faiss_config_2)
+
+    vs = VectorStore(faiss_config)
 
     nb_vectors = 1000
     k = 4
 
-    db_vectors = T.rand(nb_vectors, faiss_config_1.dim).cuda()
-    vs_1.add(db_vectors)
-    vs_2.add(db_vectors)
+    db_vectors = T.rand(nb_vectors, faiss_config.dim).cuda()
+    vs.add(db_vectors)
 
-    query_vector = T.rand(1, faiss_config_1.dim).cuda()
+    query_vector = T.rand(1, faiss_config.dim).cuda()
     logger.info(f"Query Vector: {query_vector}")
-    D_1, I_1 = vs_1.search(query_vector, k)
+    D_1, I_1 = vs.search(query_vector, k)
 
     logger.info(f"L2 Distances: {D_1}")
     logger.info(f"Indices: {I_1}")
-
-    D_2, I_2 = vs_2.search(query_vector, k)
-
-    logger.info(f"IP Distances: {D_2}")
-    logger.info(f"Indices: {I_2}")
 
 
 if __name__ == "__main__":
